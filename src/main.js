31c80cc719bb1e27c32e2e91ff7035b231627664
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-12 23:06:25 +0530

    save changes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-import {} from "node:fs";
+import {MongoClient} from "mongodb";
+
+async function main(){
+    const uri = "mongodb://127.0.0.1:27017";
+
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+    const messagecoll = db.collection("message");
+    
+    await client.close();
+}
